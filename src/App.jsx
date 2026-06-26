370676cf845c7682335145dd547a6dad350641c3
Akshay Jha <akshayjha2006@gmail.com>
2026-06-27 00:22:03 +0530

    Auto Login

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,20 @@
+import { useEffect } from 'react';
 import AppRoutes from './routes/AppRoutes';
+import useAuthStore from './store/authStore';
 
 function App() {
+  const fetchCurrentUser = useAuthStore((state) => state.fetchCurrentUser);
+
+  useEffect(() => {
+    const token = localStorage.getItem('hacknest_token');
+    if (token) {
+      // Auto-login flow: fetch user profile if a token exists on startup
+      fetchCurrentUser().catch(() => {
+        // Errors (like 401s) are natively caught by fetchCurrentUser to trigger a logout
+      });
+    }
+  }, [fetchCurrentUser]);
+
   return <AppRoutes />;
 }
 
