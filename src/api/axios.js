2fd7bb4ee7b9e2c51239727efd8f87bfe68f419a
Akshay Jha <akshayjha2006@gmail.com>
2026-06-26 23:57:46 +0530

    Router Setup

---
+++ src/api/axios.js
@@ -0,0 +1,32 @@
+import axios from 'axios';
+
+const api = axios.create({
+  baseURL: import.meta.env.VITE_API_BASE_URL,
+  headers: {
+    'Content-Type': 'application/json',
+  },
+});
+
+// Request interceptor
+api.interceptors.request.use(
+  (config) => {
+    // Future request configurations (e.g., JWT token) will go here
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
+// Response interceptor
+api.interceptors.response.use(
+  (response) => {
+    return response;
+  },
+  (error) => {
+    // Global error handling can be placed here
+    return Promise.reject(error);
+  }
+);
+
+export default api;
